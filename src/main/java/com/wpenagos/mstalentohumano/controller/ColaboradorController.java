cf21df56b5ce842e7f689c4876069ec74d099404
wilson penagos <wpenagos0833@gmail.com>
2026-06-12 10:26:10 -0500

    Agregando configuración CORS

--- src/main/java/com/wpenagos/mstalentohumano/controller/ColaboradorController.java
+++ src/main/java/com/wpenagos/mstalentohumano/controller/ColaboradorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/colaboradores")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ColaboradorController {
 
     @Autowired
