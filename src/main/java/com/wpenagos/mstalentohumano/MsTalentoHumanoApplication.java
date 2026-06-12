9ce8bcf1ce9a70c38311431acc190d548d990ee9
wilson penagos <wpenagos0833@gmail.com>
2026-06-12 12:18:37 -0500

    Mejorando configuracion CORS

--- src/main/java/com/wpenagos/mstalentohumano/MsTalentoHumanoApplication.java
+++ src/main/java/com/wpenagos/mstalentohumano/MsTalentoHumanoApplication.java
@@ -2,6 +2,9 @@ package com.wpenagos.mstalentohumano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsTalentoHumanoApplication {
@@ -10,4 +13,17 @@ public class MsTalentoHumanoApplication {
         SpringApplication.run(MsTalentoHumanoApplication.class, args);
     }
 
+    // Agrega este Bean aquí mismo
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://talento-humano-frontend.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
--- src/main/java/com/wpenagos/mstalentohumano/controller/ColaboradorController.java
+++ src/main/java/com/wpenagos/mstalentohumano/controller/ColaboradorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/colaboradores")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ColaboradorController {
 
     @Autowired
